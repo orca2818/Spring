6bf905d625d14fd0b2a9bd2df8f342ade9e01507
suzuki <suzuki@ncs.ac.jp>
2021-06-18 11:52:59 +0900

    Create Project

---
+++ src/main/java/ncs/r4a118/LecSpringApplication.java
@@ -0,0 +1,13 @@
+package ncs.r4a118;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LecSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ncs/r4a118/LecSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ncs.r4a118;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LecSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
