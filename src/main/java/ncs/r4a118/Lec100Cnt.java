a8185b4a87b1e06eeb8f8776c6793db29c551d24
suzuki <suzuki@ncs.ac.jp>
2021-06-18 12:33:00 +0900

    Update Project(2021/06/18 12:32)

--- src/main/java/ncs/r4a118/Lec100Cnt.java
+++ src/main/java/ncs/r4a118/Lec100Cnt.java
@@ -1,13 +1,16 @@
 package ncs.r4a118;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Lec100Cnt {
 
 	@RequestMapping("/lec1")
-	public String show() {
+	public String show(Model model) {
+		model.addAttribute("title1","Spring Boot");
+		model.addAttribute("title2","Thymeleafについて");
 		return "/lec1/lec101.html";
 	}
 
---
+++ src/main/java/ncs/r4a118/shop/Product.java
@@ -0,0 +1,94 @@
+package ncs.r4a118.shop;
+
+public class Product {
+	private int code;
+	private String name;
+	private int price;
+	private String unit;
+	private String desc;
+	private String img;
+
+	public Product() {}
+
+	public Product(int code, String name, int price, String unit) {
+		super();
+		this.code = code;
+		this.name = name;
+		this.price = price;
+		this.unit = unit;
+	}
+
+	public Product(int code, String name, int price, String unit, String desc, String img) {
+		super();
+		this.code = code;
+		this.name = name;
+		this.price = price;
+		this.unit = unit;
+		this.desc = desc;
+		this.img = img;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Product [code=");
+		builder.append(code);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append(", price=");
+		builder.append(price);
+		builder.append(", unit=");
+		builder.append(unit);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
