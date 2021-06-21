f2b56f54fa45ddf7e3859c4d77479fb714bc3ba9
suzuki <suzuki@ncs.ac.jp>
2021-06-21 10:32:17 +0900

    Update Project(2021/06/21 10:32)

--- src/main/java/ncs/r4a118/Lec100Cnt.java
+++ src/main/java/ncs/r4a118/Lec100Cnt.java
@@ -1,16 +1,38 @@
 package ncs.r4a118;
 
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ncs.r4a118.shop.Product;
+
 @Controller
 public class Lec100Cnt {
+	private static final String RESOURCE_NAME = "messages";
+	private static ResourceBundle resource = ResourceBundle.getBundle(RESOURCE_NAME);
 
 	@RequestMapping("/lec1")
 	public String show(Model model) {
+		//タイトル
 		model.addAttribute("title1","Spring Boot");
 		model.addAttribute("title2","Thymeleafについて");
+
+		//オブジェクト
+		var product = new Product(101,"チョコ",300,"１箱");
+		model.addAttribute("product",product);
+
+		//オブジェクトのリスト
+		var producutList = new ArrayList<Product>();
+		producutList.add(new Product(101,"チョコ",300,"１箱"));
+		producutList.add(new Product(102,"ガム",100,"１箱"));
+		producutList.add(new Product(103,"グミ",200,"１袋"));
+		model.addAttribute("productList", producutList);
+
+		//プロパティ
+		model.addAttribute("msg",resource.getString("msg"));
 		return "/lec1/lec101.html";
 	}
 
