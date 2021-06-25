033df188d8a7a6345e461b81ca8c6415cbb81d9a
suzuki <suzuki@ncs.ac.jp>
2021-06-25 12:27:11 +0900

    Project Update (2021/06/25 12:27)

--- src/main/java/ncs/r4a118/shop/ProductControl.java
+++ src/main/java/ncs/r4a118/shop/ProductControl.java
@@ -1,8 +1,11 @@
 package ncs.r4a118.shop;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -27,5 +30,40 @@ public class ProductControl {
 		model.addAttribute(new Product());
 		return "/shop/pro_insert";
 	}
+
+	@RequestMapping("/insert_confirm")
+	public String insert(@Valid Product product, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return "/shop/pro_insert";
+		}
+		manage.insert(product);
+		return select(model);
+	}
+
+	@RequestMapping("/search")
+	public String search(Product product, Model model) {
+		model.addAttribute("title", "Spring boot データベース");
+		model.addAttribute("subTitle", "Update Product");
+		model.addAttribute("searchItem", manage.search(product));
+		return "/shop/pro_update";
+	}
+
+	@RequestMapping(value = "/update", params = "update")
+	public String update(@Valid Product product, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return search(product,model);
+		}
+		manage.update(product);
+		return select(model);
+	}
+
+
+	@RequestMapping(value = "/update", params = "delete")
+	public String update(Product product, Model model) {
+		manage.delete(product);
+		return select(model);
+	}
+
+
 
 }
--- src/main/java/ncs/r4a118/shop/ProductDBManage.java
+++ src/main/java/ncs/r4a118/shop/ProductDBManage.java
@@ -18,6 +18,10 @@ public class ProductDBManage extends JdbcDaoSupport implements DBDao<Product> {
 
 	//sql
 	private final String selectSql = "select pro_code, pro_name, pro_price, pro_unit, pro_desc, pro_img from products";
+	private final String seacrhSql = "select pro_code, pro_name, pro_price, pro_unit, pro_desc, pro_img from products where pro_code=?";
+	private final String insertSql = "insert into products(pro_name, pro_price, pro_unit, pro_desc, pro_img) values(?,?,?,?,?)";
+	private final String deleteSql = "delete from products where pro_code=?";
+	private final String updateSql = "update products set pro_name=?, pro_price=?, pro_unit=?, pro_desc=?, pro_img=? where pro_code=?";
 
 	@Autowired
 	private DataSource dataSource;
@@ -51,26 +55,47 @@ public class ProductDBManage extends JdbcDaoSupport implements DBDao<Product> {
 	}
 
 	@Override
-	public int insert(Product type) throws DataAccessException {
-		return 0;
+	public int insert(Product product) throws DataAccessException {
+		Object[] param = {
+				product.getName(),
+				product.getPrice(),
+				product.getUnit(),
+				product.getDesc(),
+				product.getImg()};
+		return getJdbcTemplate().update(insertSql, param);
 	}
 
 	@Override
-	public Product search(Product type) throws DataAccessException {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+	public Product search(Product product) throws DataAccessException {
+		RowMapper<Product> map = (rs,rowNum) -> {
+			Product item = new Product();
+			item.setCode(rs.getInt("pro_code"));
+			item.setName(rs.getString("pro_name"));
+			item.setPrice(rs.getInt("pro_price"));
+			item.setUnit(rs.getString("pro_unit"));
+			item.setDesc(rs.getString("pro_desc"));
+			item.setImg(rs.getString("pro_img"));
+			return item;
+		};
+		return getJdbcTemplate().query(seacrhSql, new Object[] {product.getCode()}, map).get(0);
 	}
 
 	@Override
-	public int delete(Product type) throws DataAccessException {
-		// TODO 自動生成されたメソッド・スタブ
-		return 0;
+	public int delete(Product product) throws DataAccessException {
+		Object[] param = {product.getCode()};
+		return getJdbcTemplate().update(deleteSql, param);
 	}
 
 	@Override
-	public int update(Product type) throws DataAccessException {
-		// TODO 自動生成されたメソッド・スタブ
-		return 0;
+	public int update(Product product) throws DataAccessException {
+		Object[] param = {
+				product.getName(),
+				product.getPrice(),
+				product.getUnit(),
+				product.getDesc(),
+				product.getImg(),
+				product.getCode()};
+		return getJdbcTemplate().update(updateSql, param);
 	}
 
 }
